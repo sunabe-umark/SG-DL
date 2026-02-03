0cba4e9697e338e5ac3100d6e95205e379fb91ab
katsuya Furugen <sunabe@u-mark.co.jp>
2026-02-03 11:03:44 +0900

    tests/login-ogout.ts

---
+++ tests/login-ogout.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://devdlpro.proto-dataline.com/');
+
+  // タイトルが「Example Domain」であることを確認
+  await expect(page).toHaveTitle('Example Domain');
+
+  await page.getByRole('textbox', { name: 'ログインID' }).click();
+  await page.getByRole('textbox', { name: 'ログインID' }).fill('tst0004');
+
+  await expect(page.getByRole('textbox', { name: 'ログインID' })).toHaveValue('tst0004');
+  await page.getByRole('textbox', { name: 'パスワード' }).click();
+  await page.getByRole('textbox', { name: 'パスワード' }).fill('tst0004');
+  await expect(page.getByRole('textbox', { name: 'パスワード' })).toHaveValue('tst0004');
+
+  await expect(page.getByRole('checkbox')).not.toBeChecked();
+
+  await page.getByRole('checkbox').check();
+  await expect(page.getByRole('checkbox')).toBeChecked();
+
+
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  //10秒まつ
+  await page.waitForTimeout(5000);
+  await page.getByRole('link', { name: 'ログアウト' }).click();
+
+   //10秒まつ
+  await page.waitForTimeout(5000);
+  await page.getByRole('button', { name: 'ログイン' }).click();
+});
