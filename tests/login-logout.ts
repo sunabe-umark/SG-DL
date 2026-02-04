641559138a132fce5cd242e10a955a00443df2cc
katsuya Furugen <sunabe@u-mark.co.jp>
2026-02-04 10:01:17 +0900

    2602041001、コミット

---
+++ tests/login-logout.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://devdlpro.proto-dataline.com/');
+
+   //5秒まつ
+  await page.waitForTimeout(6000);
+
+  // タイトルが「Example Domain」であることを確認
+  await expect(page).toHaveTitle('Example DomainPRO（データラインプロ）法人向け中古車データ検索');
+
+  await page.getByRole('textbox', { name: 'ログインID' }).click();
+  await page.getByRole('textbox', { name: 'ログインID' }).fill('tst0004');
+
+  await expect(page.getByRole('textbox', { name: 'ログインID' })).toHaveValue('tst0004');
+  await page.getByRole('textbox', { name: 'パスワード' }).click();
+  await page.getByRole('textbox', { name: 'パスワード' }).fill('tst0004');
+  await expect(page.getByRole('textbox', { name: 'パスワード' })).toHaveValue('tst0004');
+
+  await expect(page.getByRole('checkbox')).not.toBeChecked();
+
+  await page.getByRole('checkbox').check();
+  await expect(page.getByRole('checkbox')).toBeChecked();
+
+
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  //5秒まつ
+  await page.waitForTimeout(5000);
+  await page.getByRole('link', { name: 'ログアウト' }).click();
+
+   //5秒まつ
+  await page.waitForTimeout(5000);
+  await page.getByRole('button', { name: 'ログイン' }).click();
+});
